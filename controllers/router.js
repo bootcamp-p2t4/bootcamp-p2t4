98d5626e267aa7c83ce9ce3dbae31780175a41bb
Jarret Yingling <44506154+JarretYingling@users.noreply.github.com>
2019-03-11 22:57:18 -0400

    commit to master to branch to jarret

--- controllers/router.js
+++ controllers/router.js
@@ -22,10 +22,12 @@ module.exports = function (app) {
     log(`__dirname: ${__dirname}`);
     // create user
     const userPassword = logic.getRandom(100000, 999999);
-    db.tbl_users.findOrCreate({where: {
-      user_name: req.body.user_name,
-      user_password: userPassword
-    }}).then(function (sqlUser) {
+    db.tbl_users.findOrCreate({
+      where: {
+        user_name: req.body.user_name,
+        user_password: userPassword
+      }
+    }).then(function (sqlUser) {
       log(logic.parseSequelize(sqlUser));
       // respond with stocks as 0 positions
       db.tbl_stocks.findAll({
@@ -42,17 +44,32 @@ module.exports = function (app) {
     });
   });
 
+  // GET /positions route
+  app.get("/positions", function (req, res) {
+    log(req.url);
+    db.tbl_stocks.findAll({
+      where: {
+        monthly_period: 1
+      }
+    }).then(function (sqlStocks) {
+      sqlStocks = logic.parseSequelize(sqlStocks);
+      log(sqlStocks);
+      res.render("index", {
+        positions: sqlStocks
+      });
+      //res.json(sqlStocks);
+    });
+  });
+
   // POST /positions route
   app.post("/positions", function (req, res) {
-    log(req.url);
+    log(req.body);
     db.tbl_stocks.findAll({}).then(function (sqlStocks) {
       sqlStocks = logic.parseSequelize(sqlStocks);
       log(sqlStocks);
       res.render("index", {
         positions: sqlStocks
       });
-
-
       //res.json(sqlStocks);
     });
   });
@@ -78,50 +95,19 @@ module.exports = function (app) {
   app.get("/stocks", function (req, res) {
     log(req.url);
     log(`__dirname: ${__dirname}`);
-    db.tbl_stocks.findAll({}).then(function (sqlStocks) {
-      //log(sqlStocks);
+    db.tbl_stocks.findAll({
+      where: {
+        monthly_period: 1
+      }
+    }).then(function (sqlStocks) {
       sqlStocks = logic.parseSequelize(sqlStocks);
       log(sqlStocks);
       /*
       res.render("index", {
-        stocks: sqlStocks
+        positions: sqlStocks
       });
       */
       res.json(sqlStocks);
-    });
-  });
-
-  // GET /prices route
-  app.get("/prices", function (req, res) {
-    log(req.url);
-    log(`__dirname: ${__dirname}`);
-    db.tbl_prices.findAll({}).then(function (sqlPrices) {
-      //log(sqlPrices);
-      sqlPrices = logic.parseSequelize(sqlPrices);
-      log(sqlPrices);
-      /*
-      res.render("index", {
-        stocks: tblStocksResult
-      });
-      */
-      res.json(sqlPrices);
-    });
-  });
-
-  // GET /positions route
-  app.get("/positions", function (req, res) {
-    log(req.url);
-    log(`__dirname: ${__dirname}`);
-    db.tbl_positions.findAll({}).then(function (sqlPositions) {
-      //log(sqlPositions);
-      sqlPositions = logic.parseSequelize(sqlPositions);
-      log(sqlPositions);
-      /*
-      res.render("index", {
-        stocks: tblStocksResult
-      });
-      */
-      res.json(logic.parseSequelize(sqlPositions));
     });
   });
 
@@ -146,21 +132,21 @@ module.exports = function (app) {
   app.post("/buy", function (req, res) {
     log(req.body);
     log(`__dirname: ${__dirname}`);
-
+    res.redirect("/positions");
   });
 
   // POST /sell route
   app.post("/sell", function (req, res) {
     log(req.body);
     log(`__dirname: ${__dirname}`);
-
+    res.redirect("/positions");
   });
 
   // POST /next route
   app.post("/next", function (req, res) {
     log(req.body);
     log(`__dirname: ${__dirname}`);
-
+    //res.redirect("/positions");
   });
 
   /*
@@ -213,6 +199,6 @@ module.exports = function (app) {
     });
   });
 
-*/
+  */
 
 };

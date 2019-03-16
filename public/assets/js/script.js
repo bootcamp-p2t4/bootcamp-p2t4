72005f7aa95999611cd8ba59171bb2099c1cc03e
Kevin Patel <kpatel1209@gmail.com>
2019-03-15 20:19:20 -0400

    chart.js changes (working), css changes to handlebars and html files, new div for cash available

--- public/assets/js/script.js
+++ public/assets/js/script.js
@@ -1,33 +1,50 @@
 const log = console.log;
-// log("Hello World");
 
 $(document).ready(function () {
 
+  // Click function to save user login to local storage, but it currently will not send the user to the positions page.
+  // Why? Route issue?
+  $("#btn-SignIn").on("click", function (event) {
+    event.preventDefault();
+
+    let newUserLogin = {
+      user_name: $("#inputName").val().trim(),
+      user_email: $("#inputEmail").val().trim(),
+      user_password: $("#inputPassword").val().trim()
+    };
+
+    log(newUserLogin);
+
+    localStorage.clear();
+
+    localStorage.setItem("user_name", newUserLogin.user_name);
+    localStorage.setItem("user_email", newUserLogin.user_email);
+    localStorage.setItem("user_password", newUserLogin.user_password);
+
+    $.post("/login", newUserLogin, function (data) {
+      if (data) {
+        log("New user login successful!");
+      } else {
+        log("Something went wrong!");
+      }
+      $("#inputName").val("");
+      $("#inputEmail").val("");
+      $("#inputPassword").val("");
+    });
+  });
 
 
 
 
-  
 
 
-
-
-    
 });
 
-// Typewriter effect for Welcome Statement when page loads
-// let t = 0;
-// let text = "This application is designed to simulate basic stock transactions over a fixed series of historical 30-day monthly periods. You start with $100,000 cash and have a list of stocks and indexes (basket of stocks) to choose from. The prices on screen are historical prices from that respective period. Your only limitation to buying and selling is your cash balance. Once your done trading for a specific period, click the ‘Next Period’ button to advance and see the new prices and your new portfolio valuation. The full simulation is 12 periods long. After you complete all 12 periods click the ‘Create Chart” button to see a visualization of your stock portfolio's performance over the given time series.";
-// let speed = 25;
-// typeWriter();
-
-// function typeWriter() {    
-//   if (t < text.length) {
-//     document.getElementById("introStatement").innerHTML += text.charAt(t);
-//     t++;
-//     setTimeout(typeWriter, speed);
-//   }
-// }
+// Disabled buy/sell button if input is empty
+// $("#buy, #sell").attr("disabled", true);
+// $("#buy_shares").keyup(function() {
+//   $("#buy, #sell").prop("disabled", this.value == "" ? true : false);
+// });
 
 // Function to buy stock
 // for (let i = 0; i < 11; i++) {
@@ -84,4 +101,18 @@ $(document).ready(function () {
 //     }
 //     $("#sell_shares").val("");
 
+// }
+
+// Typewriter effect for Welcome Statement when page loads
+// let t = 0;
+// let text = "This application is designed to simulate basic stock transactions over a fixed series of historical 30-day monthly periods. You start with $100,000 cash and have a list of stocks and indexes (basket of stocks) to choose from. The prices on screen are historical prices from that respective period. Your only limitation to buying and selling is your cash balance. Once your done trading for a specific period, click the ‘Next Period’ button to advance and see the new prices and your new portfolio valuation. The full simulation is 12 periods long. After you complete all 12 periods click the ‘Create Chart” button to see a visualization of your stock portfolio's performance over the given time series.";
+// let speed = 25;
+// typeWriter();
+
+// function typeWriter() {    
+//   if (t < text.length) {
+//     document.getElementById("introStatement").innerHTML += text.charAt(t);
+//     t++;
+//     setTimeout(typeWriter, speed);
+//   }
 // }
